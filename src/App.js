97894ffc1c6f1491ac2db709b6c02bcad8d5da6e
precious success <122813633+preciousprestige@users.noreply.github.com>
2025-06-25 22:13:00 +0800

    Fix homepage and routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Intro from './Intro';
 import ComingSoon from './ComingSoon';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/coming-soon">
       <Routes>
-        <Route path="/" element={<Intro />} />              {/* First page */}
-        <Route path="/coming-soon" element={<ComingSoon />} />  {/* Second page */}
+        <Route path="/" element={<Intro />} />
+        <Route path="/coming-soon" element={<ComingSoon />} />
       </Routes>
     </Router>
   );
